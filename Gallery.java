00f7b53b8482491f621ded5fcccb3239793d9f34
purrsPoint <felipedossantosleonardo@gmail.com>
2025-06-15 19:03:08 -0300

    making button

--- Gallery.java
+++ Gallery.java
@@ -1,16 +1,26 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class Gallery {
+public class Gallery{
+
+    private JButton chooser_button;
 
     public void makeScreen(){
         JFrame Screen = new JFrame ("Navegador de imagens com galeria");
 
-        
 
         Screen.setVisible(true);
+        Screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Screen.setBounds(0,0,800,500);
         Screen.setLocationRelativeTo(null);
-        Screen.setLayout(new FlowLayout());
+
+        //Construindo botão
+        chooser_button.setText("Selecionar foto");
+        chooser_button.setBounds(0,0,50,50);
+
+
+        //Adicionando componentes 
+        Screen.getContentPane().add(chooser_button);
 
     }
 }
